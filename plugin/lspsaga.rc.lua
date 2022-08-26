7daafbfa4332fb31ba84388eeb541c1cce182001
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-08-26 18:44:34 +0100

    feat: added jump diagnostic and diagnostic line commands from lspsaga plugin

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -1,5 +1,6 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
+local opts = { noremap = true, silent = true }
 
 saga.init_lsp_saga {
   server_filetype_map = {
@@ -7,11 +8,12 @@ saga.init_lsp_saga {
   }
 }
 
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-d>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-d>', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
 vim.keymap.set('n', '<S-k>', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', '<S-t>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set('n', 'ga', '<Cmd>Lspsaga code_action<CR>', opts)
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
